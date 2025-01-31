0a68cfd299797327f9e52c27d48b6c5199ce4be4
Bruno Oliveira <brunooliveira812@gmail.com>
2025-01-30 22:28:23 -0300

    remove dist folder

--- discord/dist/types/discord.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Discord {
-    private client;
-    private token;
-    constructor(token: string);
-    login(): Promise<void>;
-    sendMessage(channelId: string, message: string): Promise<void>;
-    shutdown(): Promise<void>;
-}
--- trace/dist/types/trace.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Trace {
-    start(): void;
-    end(): void;
-    getParent(): Trace | null;
-    getName(): string;
-}
-export declare function setTrace(fn: (name: string, parent: Trace | null) => Trace): void;
-export declare function trace<T>(name: string, fn: () => Promise<T>): Promise<T>;
